eafaa7bc5362d1e108d487bc43c24b3884c49f75
imsang27 <69525587+imsang27@users.noreply.github.com>
2021-02-01 01:28:26 +0900

    Create Lotto.py

---
+++ Lotto.py
@@ -0,0 +1,22 @@
+import random
+
+print("Do you want to Sort?\n")
+Y = 'Y', 'y'
+N = 'N', 'n'
+YN = input("Please enter Y or N.\n\n")
+Lotto = []
+
+
+if YN == Y :
+  while len(Lotto) < 7 :
+    num = random.randint(1, 46)
+    print(sorted(num))
+
+
+elif YN == N :
+  while len(Lotto) < 7 :
+    num = random.randint(1, 46)
+    print(num)
+
+if YN != Y or N :
+  print('Please enter Y or N only.')
